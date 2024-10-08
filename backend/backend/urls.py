98268d109004df9fcd874aa6b908a4009b81915e
Халиков Ульфат Жэудатович <u.z.khalikov@onegroup.ru>
2024-10-08 15:59:42 +0300

    change nginx conf and urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,7 +4,7 @@ from backend import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('adminpan/', admin.site.urls),
     path('api/users/', include('users.urls')),
     path('api/', include('api.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
