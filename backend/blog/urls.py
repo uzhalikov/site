dff1e90b62f9c8cc1d1c4cfc07ba4b59536bb8ce
Халиков Ульфат Жэудатович <u.z.khalikov@onegroup.ru>
2024-10-10 14:53:33 +0300

    change urls

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -8,12 +8,12 @@ from django.contrib.sitemaps.views import sitemap
 
 urlpatterns = [
     path('', about, name='about'),
-    path('questions', questions, name='questions'),
-    path('posts', posts, name='posts'),
-    path('poems', poems, name='poems'),
-    path('post/<slug:slug>', show_post, name='post'),
-    path('poem/<int:poem_id>', show_poem, name='poem'),
-    path('api/create_question', create_question),
-    path('api/questions', get_all_questions),
+    path('questions/', questions, name='questions'),
+    path('posts/', posts, name='posts'),
+    path('poems/', poems, name='poems'),
+    path('post/<slug:slug>/', show_post, name='post'),
+    path('poem/<int:poem_id>/', show_poem, name='poem'),
+    path('api/create_question/', create_question),
+    path('api/questions/', get_all_questions),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}),
 ]
